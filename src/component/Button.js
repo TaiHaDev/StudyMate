3e07c62d476abd795adbc5aed29872cd8ddf9b59
Tai Ha <u7454578@anu.edu.au>
2023-07-18 20:58:14 +1000

    darkmode toggle

--- src/component/Button.js
+++ src/component/Button.js
@@ -8,7 +8,7 @@ const Button = ({iconSrc, description, onClickHandler, isTop}) => {
   const onLeaveHandler = event => {
     descriptionRef.current.classList.add('invisible')
   }
-  const positionClass = isTop ? 'bg-slate-100 rounded-md py-0.5 px-1 text-xs font-medium absolute -top-6 left-1/2 -translate-x-1/2 invisible' : 'bg-slate-100 rounded-md py-0.5 px-1 text-xs font-medium absolute -bottom-6 left-1/2 -translate-x-1/2 invisible' 
+  const positionClass = isTop ? 'bg-slate-100 dark:bg-black dark:text-white rounded-md py-0.5 px-1 text-xs font-medium absolute -top-6 left-1/2 -translate-x-1/2 invisible' : 'bg-slate-100 rounded-md dark:bg-black dark:text-white py-0.5 px-1 text-xs font-medium absolute -bottom-6 left-1/2 -translate-x-1/2 invisible' 
 
   return (
     <div className='relative'   onMouseEnter={onHoverHandler} onMouseLeave={onLeaveHandler} onClick={onClickHandler}>
